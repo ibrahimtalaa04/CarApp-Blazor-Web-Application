af88b48e7f9c59d213a3f2b355cb5669c5011372
ibrahimtalaa04 <ibrahimtalaa04@gmail.com>
2023-10-02 22:53:02 +0300

    config ef with database connection

---
+++ CarApp.Web/GlobalUsing.cs
@@ -0,0 +1,14 @@
+﻿global using CarApp.Data;
+global using CarApp.Web.Data;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.EntityFrameworkCore;
+global using CarApp.Infrastructure.Repository;
+global using CarApp.Infrastructure.Repository.IRepository;
+
+namespace CarApp.Web
+{
+    public class GlobalUsing
+    {
+    }
+}
--- CarApp.Web/Program.cs
+++ CarApp.Web/Program.cs
@@ -1,13 +1,26 @@
-using CarApp.Web.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddDbContext<DatabaseContext>(opts =>
+{
+    opts.UseSqlServer(connectionString, options =>
+    {
+        options.MigrationsAssembly("CarApp.Data");
+    });
+    opts.EnableDetailedErrors();
+    opts.EnableSensitiveDataLogging();
+});
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>(); //
+
 
 var app = builder.Build();
 
